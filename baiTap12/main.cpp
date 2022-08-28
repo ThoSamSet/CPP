669a4d002f3cc291882c56f5cf6afdfebfe8db5f
Finch <puansuanzun@gmail.com>
2022-08-28 22:24:07 +0900

    Thêm bài tập 12

---
+++ baiTap12/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    // Tạo biến
+    // cách
+    int w = 0;
+
+    // Nhập cách
+    cout << "Nhập cách:" << endl;
+    cin >> w;
+
+    // Rẽ nhánh chương trình 
+    switch (w)
+    {
+    case 1:
+        break;
+    
+    default:
+        
+        break;
+    }
+
+    return 0;
+}

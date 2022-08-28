ab6e1c71828be45a199ec62f20697b20b7006680
Finch <puansuanzun@gmail.com>
2022-08-28 23:22:31 +0900

    Sửa bài 12

--- baiTap12/main.cpp
+++ baiTap12/main.cpp
@@ -6,23 +6,46 @@ using namespace std;
 int main()
 {
     // Tạo biến
+    int i = 1, n;
+    float x, T = 1, S = 0;
+
     // cách
-    int w = 0;
+    int cach = 0;
+
+    // Nhập dữ liệu từ bàn phím 
+    cout << "Nhập vào số x:";
+    cin >> x;
+    cout << "Nhập vào số y:";
+    cin >> n;
 
     // Nhập cách
-    cout << "Nhập cách:" << endl;
-    cin >> w;
+    cout << "Nhập cách:";
+    cin >> cach;
 
     // Rẽ nhánh chương trình 
-    switch (w)
+    switch (cach)
     {
     case 1:
+        // Sử dụng for để tính biểu thức
+        for(i = 1; i <= n; i++)
+        {
+            T = T * x;
+            S = S + T;
+        }
         break;
-    
     default:
-        
+        // Sử dụng vòng lặp while để tính biểu thức 
+        while(i <= n)
+        {
+            T = T * x;
+            S = S + T;
+            i++;
+        }
         break;
     }
+
+    // xuất kết quả
+    cout << "Tổng = " << S << endl;
 
     return 0;
 }

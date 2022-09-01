fbbd57d2c26d7a1afd722e4964eb4930972ed65b
Finch <puansuanzun@gmail.com>
2022-09-02 01:33:53 +0900

    Sửa bài tập 19

--- baiTap19/main.cpp
+++ baiTap19/main.cpp
@@ -7,7 +7,8 @@ using namespace std;
 int main()
 {
     // Tạo biến
-    long n;
+    long n, temp;
+    int count = 0;
 
     // Nhập dữ liệu từ bàn phím 
     // Số đã nhập phải lớn hơn 0
@@ -22,6 +23,21 @@ int main()
         }
     }while(n <= 0);
 
+    // Gán bién n = temp, sử dụng temp để đếm số
+    temp = n;
+    // nếu n == 0 thì n có 1 chữ số
+    if(n == 0) count = 1;
+    // Nếu temp != 0 thì sử dụng while để đếm
+    while(temp != 0)
+    {
+        // Sau mỗi vòng lặp, nếu temp là số lẻ thì count tăng thêm 1
+        if(temp % 2 == 1) count++;
+        // Chia lấy nguyên cho 10, cứ mỗi lần chia thì số n có một chữ số
+        temp = temp / 10;
+    }
+
+    // Xuất kết quả
+    cout << "Số " << n << " có " << count << " chữ số lẻ!!" << endl;
     
     return 0;
 }

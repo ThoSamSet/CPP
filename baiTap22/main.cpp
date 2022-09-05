43ea17e25c3903ca583c385c6d03000a7f80049e
Finch <puansuanzun@gmail.com>
2022-09-05 17:20:23 +0900

    Sửa bài tập 22

--- baiTap22/main.cpp
+++ baiTap22/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     // Tạo biến
-    int temp, n, i, max = 0;
+    int temp, n, i, max = 1, count = 1;
 
     // Nhập dữ liệu từ bàn phím 
     // Số đã nhập phải lớn hơn 0
@@ -15,12 +15,26 @@ int main()
     {
         cout << "Nhập vào số nguyên lớn hơn 0: ";
         cin >> n;
-        if(n <= 0)
-        {
-            cout << "Số n phải lớn hơn 0!!!" << endl;
-        }
-    }while(n <= 0);
+    }while(n < 0 && cout << "Số n phải lớn hơn 0");
 
+    temp = n;
+
+    // Nếu n == 0 thì count = 1
+    if(n == 0) count = 1;
+
+    // Sử dụng do while để tìm chữ số lớn nhất 
+    // Mỗi lần tìm thấy thi count++
+    do
+    {
+        i = n % 10;
+        if(i == max) count++;
+        if(i > max) max = i;
+    } while (n /= 10);
+
+    // In két quả
+    cout << "Chữ số lớn nhất trong " << n << "  là: " << max << endl;
+    cout << " Có " << count << " chữ số " << max << " trong số " << temp << endl;
+    
     
     return 0;
 }

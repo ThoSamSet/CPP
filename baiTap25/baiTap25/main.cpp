33393d0d279e7cba7847fd2244bf96509e1a5acf
Finch <finch@Finchs-MacBook-Air.local>
2022-10-14 07:28:25 +0900

    Thêm bài 25

---
+++ baiTap25/baiTap25/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  baiTap25
+//
+//  Created by Finch on 2022/10/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

cb3a3179b2a6538c3345ed291a7c80ecdf2de4c5
ThoSamSet <puansuanzun@gmail.com>
2022-10-14 22:38:08 +0900

    Xoá files không cần thiết

--- baiTapTienChiThi1/baiTapTienChiThi1/Define.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#define HELLO "Hello"
-#define XIN_CHAO "Xin Chao"
--- baiTapTienChiThi1/baiTapTienChiThi1/String_Fn.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Header.h"
-#include <string>
-
-//Cac ham ve chuoi - string
-string InHoa(string chuoi);
-string InThuong(string chuoi);

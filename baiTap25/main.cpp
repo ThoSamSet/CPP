8d5f45e1726bcf5ec146123141fc6807a732df7a
ThoSamSet <puansuanzun@gmail.com>
2022-10-17 09:41:16 +0900

    Thêm định nghĩa các hàm tính toán và chuỗi

--- baiTap25/main.cpp
+++ baiTap25/main.cpp
@@ -1,7 +1,60 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+//Các hàm về tính toán - Math
+//Ước chung lớn nhất
+int UCLN(int a, int b)
+{
+    if (a == b) return a;
+	else if (a > b) return UCLN(a - b, b);
+	else return UCLN(a, b - a);
+};
+
+//Bội chung nhỏ nhất
+int BCNN(int a, int b)
+{
+    int uocChung = UCLN(a, b);
+	return a * b / uocChung;
+};
+
+//Kiểm tra số nguyên tố
+bool IsSoNguyenTo(int n)
+{
+    if (n < 2) return false;
+	else if (n == 2) return true;
+	else
+	{
+		int num = sqrt(n);
+		for (int i = 2; i <= num; i++)
+		{
+			if (n % i == 0) return false;
+		}
+		return true;
+	}
+};
+
+//Các hàm về chuỗi - String
+string InHoa(string chuoi)
+{
+    for (int i = 0; i < chuoi.size(); i++)
+	{
+		if (chuoi[i] >= 'a' && chuoi[i] <= 'z') chuoi[i] = chuoi[i] - 'a' + 'A';
+	}
+	return chuoi;
+};
+
+string InThuong(string chuoi)
+{
+    for (int i = 0; i < chuoi.size(); i++)
+	{
+		if (chuoi[i] >= 'A' && chuoi[i] <= 'Z') chuoi[i] = chuoi[i] - 'A' + 'a';
+	}
+	return chuoi;
+};
+
+//Lớp ConNguoi
 class ConNguoi
 {
     string hoTen;
